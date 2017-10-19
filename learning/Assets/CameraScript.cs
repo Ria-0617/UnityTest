91922ebf0a07b4bd05f097e76dcdf9df81a1a58a
ps16atanaka <kyelese0617@gmail.com>
2017-10-20 01:47:38 +0900

    追加：カメラの向きに合わせて移動 途中：カメラ実装

---
+++ learning/Assets/CameraScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+    public float m_speed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var h = Input.GetAxisRaw("Horizontal");
+        var v = Input.GetAxisRaw("Vertical");
+
+        if (h != 0 || v != 0)
+        {
+            var position = transform.position;
+            var cameraTransform = Camera.main.transform;
+            var addValue = (cameraTransform.forward * v + cameraTransform.right * h).normalized * Time.deltaTime * m_speed;
+            addValue.y = 0;
+            position += addValue;
+            transform.position = position;
+        }
+    }
+
+}
